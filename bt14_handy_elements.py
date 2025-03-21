c9b6106ba8078743293b2927e80f0480c92b443f
unknown <leminhtu74@gmail.com>
2025-03-21 23:33:41 +0700

    feat: complete tutorial 14

---
+++ bt14_handy_elements.py
@@ -0,0 +1,8 @@
+summary = """
+Check out the GStreamer Handy Elements tutorial:
+https://gstreamer.freedesktop.org/documentation/tutorials/basic/handy-elements.html?gi-language=c
+
+It just lists some common and useful elements—no C or Python code here! Better read the source. 😊🚀
+"""
+
+print(summary)
