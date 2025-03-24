00a76fcdb7be3db1685cd3404ffa4e4b42a3bb5a
unknown <leminhtu74@gmail.com>
2025-03-24 21:43:53 +0700

    feat: add gstreamer url + refactor

--- bt14_handy_elements.py
+++ bt14_handy_elements.py
@@ -1,3 +1,8 @@
+"""
+This Python script is based on the GStreamer tutorial:
+https://gstreamer.freedesktop.org/documentation/tutorials/basic/handy-elements.html?gi-language=python
+"""
+
 summary = """
 Check out the GStreamer Handy Elements tutorial:
 https://gstreamer.freedesktop.org/documentation/tutorials/basic/handy-elements.html?gi-language=c
