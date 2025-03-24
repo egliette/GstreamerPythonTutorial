426a126fd599e2349ae03499275cd1ee9d382bb1
unknown <leminhtu74@gmail.com>
2025-03-24 21:35:28 +0700

    feat: add gstreamer url + refactor

--- bt11_debugging_tools.py
+++ bt11_debugging_tools.py
@@ -1,75 +1,91 @@
-#!/usr/bin/env python3
+"""
+This Python script is based on the GStreamer tutorial:
+https://gstreamer.freedesktop.org/documentation/tutorials/basic/debugging-tools.html?gi-language=python
+"""
+
+import os
 
 # Debug log: Show error and log messages by setting environment variable
-import os
 os.environ["GST_DEBUG"] = "2,glimagesink:0,basesrc:0"
 
-import sys
-import subprocess
 import logging
-logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)s] - %(message)s")
+import subprocess
+import sys
+
+logging.basicConfig(
+    level=logging.DEBUG, format="[%(name)s] [%(levelname)s] - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
 import gi
-gi.require_version('Gst', '1.0')
+
+gi.require_version("Gst", "1.0")
 from gi.repository import Gst
 
 
-# initialize GStreamer
-Gst.init(sys.argv[1:])
+def main():
+    # initialize GStreamer
+    Gst.init(sys.argv[1:])
 
-# Create the elements
-source = Gst.ElementFactory.make("videotestsrc", "source")
-vertigotv = Gst.ElementFactory.make("vertigotv", "vertigotv")
-videoconvert = Gst.ElementFactory.make("videoconvert", "videoconvert")
-sink = Gst.ElementFactory.make("autovideosink", "sink")
+    # Create the elements
+    source = Gst.ElementFactory.make("videotestsrc", "source")
+    vertigotv = Gst.ElementFactory.make("vertigotv", "vertigotv")
+    videoconvert = Gst.ElementFactory.make("videoconvert", "videoconvert")
+    sink = Gst.ElementFactory.make("autovideosink", "sink")
 
-# Build the pipeline
-pipeline = Gst.Pipeline.new("test-pipeline")
-pipeline.add(source)
-pipeline.add(vertigotv)
-pipeline.add(videoconvert)
-pipeline.add(sink)
+    # Build the pipeline
+    pipeline = Gst.Pipeline.new("test-pipeline")
+    pipeline.add(source)
+    pipeline.add(vertigotv)
+    pipeline.add(videoconvert)
+    pipeline.add(sink)
 
-# Link elements
-source.link(vertigotv)
-vertigotv.link(videoconvert)
-videoconvert.link(sink)
+    # Link elements
+    source.link(vertigotv)
+    vertigotv.link(videoconvert)
+    videoconvert.link(sink)
 
-# Modify the source's properties
-source.props.pattern = "ball"
-# Can alternatively be done using `source.set_property("pattern", 0)`
-# or using `Gst.util_set_object_arg(source, "pattern", 0)`
+    # Modify the source's properties
+    source.props.pattern = "ball"
+    # Can alternatively be done using `source.set_property("pattern", 0)`
+    # or using `Gst.util_set_object_arg(source, "pattern", 0)`
 
-# start playing
-pipeline.set_state(Gst.State.PLAYING)
+    # start playing
+    pipeline.set_state(Gst.State.PLAYING)
 
-# Write dot file
-dot_file = "pipeline.dot"
-with open(dot_file, 'w') as f:
-    f.write(Gst.debug_bin_to_dot_data(pipeline, Gst.DebugGraphDetails.ALL))
+    # Write dot file
+    dot_file = "pipeline.dot"
+    with open(dot_file, "w") as f:
+        f.write(Gst.debug_bin_to_dot_data(pipeline, Gst.DebugGraphDetails.ALL))
 
-png_file = dot_file.replace(".dot", ".png")
-subprocess.run(["dot", "-Tpng", dot_file, "-o", png_file])
-print(f"Saved pipeline graph as {png_file}")
+    png_file = dot_file.replace(".dot", ".png")
+    subprocess.run(["dot", "-Tpng", dot_file, "-o", png_file])
+    print(f"Saved pipeline graph as {png_file}")
 
-# wait until EOS or error
-bus = pipeline.get_bus()
-msg = bus.timed_pop_filtered(
-    Gst.CLOCK_TIME_NONE,
-    Gst.MessageType.ERROR | Gst.MessageType.EOS
-)
-# Parse message
-if msg:
-    if msg.type == Gst.MessageType.ERROR:
-        err, debug_info = msg.parse_error()
-        logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
-        logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
-    elif msg.type == Gst.MessageType.EOS:
-        logger.info("End-Of-Stream reached.")
-    else:
-        # This should not happen as we only asked for ERRORs and EOS
-        logger.error("Unexpected message received.")
-    
-# free resources
-pipeline.set_state(Gst.State.NULL)
+    # wait until EOS or error
+    bus = pipeline.get_bus()
+    msg = bus.timed_pop_filtered(
+        Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS
+    )
+    # Parse message
+    if msg:
+        if msg.type == Gst.MessageType.ERROR:
+            err, debug_info = msg.parse_error()
+            logger.error(
+                f"Error received from element {msg.src.get_name()}: {err.message}"
+            )
+            logger.error(
+                f"Debugging information: {debug_info if debug_info else 'none'}"
+            )
+        elif msg.type == Gst.MessageType.EOS:
+            logger.info("End-Of-Stream reached.")
+        else:
+            # This should not happen as we only asked for ERRORs and EOS
+            logger.error("Unexpected message received.")
+
+    # free resources
+    pipeline.set_state(Gst.State.NULL)
+
+
+if __name__ == "__main__":
+    main()
