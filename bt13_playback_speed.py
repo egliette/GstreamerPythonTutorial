8453f487adbc41023917e9f6142f40e4ca499990
unknown <leminhtu74@gmail.com>
2025-03-24 21:42:53 +0700

    feat: add gstreamer url + refactor

--- bt13_playback_speed.py
+++ bt13_playback_speed.py
@@ -1,10 +1,24 @@
+"""
+This Python script is based on the GStreamer tutorial:
+https://gstreamer.freedesktop.org/documentation/tutorials/basic/playback-speed.html?gi-language=python
+"""
+
+import os
+
+# set to 0 to show the USAGE message
+os.environ["GST_DEBUG"] = "0"
 import logging
-logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)s] - %(message)s")
+
+logging.basicConfig(
+    level=logging.DEBUG, format="[%(name)s] [%(levelname)s] - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
 import gi
-gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GLib
+
+gi.require_version("Gst", "1.0")
+from gi.repository import GLib, Gst
+
 
 class VideoPlayer:
     def __init__(self, uri):
@@ -13,7 +27,7 @@ class VideoPlayer:
         self.loop = GLib.MainLoop()
         self.playing = True
         self.rate = 1.0
-        
+
     def send_seek_event(self):
         success, position = self.pipeline.query_position(Gst.Format.TIME)
         if not success:
@@ -23,15 +37,23 @@ class VideoPlayer:
         seek_flags = Gst.SeekFlags.FLUSH | Gst.SeekFlags.ACCURATE
         if self.rate > 0:
             seek_event = Gst.Event.new_seek(
-                self.rate, Gst.Format.TIME, seek_flags,
-                Gst.SeekType.SET, position,
-                Gst.SeekType.END, position
+                self.rate,
+                Gst.Format.TIME,
+                seek_flags,
+                Gst.SeekType.SET,
+                position,
+                Gst.SeekType.END,
+                position,
             )
         else:
             seek_event = Gst.Event.new_seek(
-                self.rate, Gst.Format.TIME, seek_flags,
-                Gst.SeekType.SET, 0,
-                Gst.SeekType.SET, position
+                self.rate,
+                Gst.Format.TIME,
+                seek_flags,
+                Gst.SeekType.SET,
+                0,
+                Gst.SeekType.SET,
+                position,
             )
 
         if self.video_sink is None:
@@ -41,44 +63,53 @@ class VideoPlayer:
         logger.info(f"Current rate: {self.rate}")
 
     def handle_keyboard(self, key):
-        if key == 'p':
+        if key == "p":
             self.playing = not self.playing
-            self.pipeline.set_state(Gst.State.PLAYING if self.playing else Gst.State.PAUSED)
+            self.pipeline.set_state(
+                Gst.State.PLAYING if self.playing else Gst.State.PAUSED
+            )
             logger.info(f"Setting state to {'PLAYING' if self.playing else 'PAUSED'}")
-        elif key == 's':
+        elif key == "s":
             self.rate /= 2.0
             self.send_seek_event()
-        elif key == 'S':
+        elif key == "S":
             self.rate *= 2.0
             self.send_seek_event()
-        elif key == 'd':
+        elif key == "d":
             self.rate *= -1.0
             self.send_seek_event()
-        elif key == 'n':
+        elif key == "n":
             if self.video_sink is None:
                 self.video_sink = self.pipeline.get_property("video-sink")
-            
-            step_event = Gst.Event.new_step(Gst.Format.BUFFERS, 1, abs(self.rate), True, False)
+
+            step_event = Gst.Event.new_step(
+                Gst.Format.BUFFERS, 1, abs(self.rate), True, False
+            )
             self.video_sink.send_event(step_event)
             logger.info("Stepping one frame")
-        elif key == 'q':
+        elif key == "q":
             self.loop.quit()
 
     def run(self):
-        logger.info("USAGE: Press a key and hit enter: [P] Play/Pause, [S/s] Speed Up/Down, [D] Reverse, [N] Next Frame, [Q] Quit")
+        logger.info(
+            "USAGE: Press a key and hit enter: [P] Play/Pause, [S/s] Speed Up/Down, [D] Reverse, [N] Next Frame, [Q] Quit"
+        )
         self.pipeline.set_state(Gst.State.PLAYING)
-        
+
         while True:
             try:
                 key = input().strip()
                 self.handle_keyboard(key)
+                if key.lower() == "q":
+                    break
             except (KeyboardInterrupt, EOFError):
                 break
-        
+
         self.pipeline.set_state(Gst.State.NULL)
         if self.video_sink:
             self.video_sink.set_state(Gst.State.NULL)
-        
+
+
 if __name__ == "__main__":
     Gst.init(None)
     player = VideoPlayer("file:///app/videos/street_5min.mp4")
