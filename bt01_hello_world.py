c8a3983f681e337a4048615abd5a2f4da3714866
unknown <leminhtu74@gmail.com>
2025-03-24 19:49:22 +0700

    feat: add gstreamer url + refactor

--- bt01_hello_world.py
+++ bt01_hello_world.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+""" 
+This Python script is based on the GStreamer tutorial:
+https://gstreamer.freedesktop.org/documentation/tutorials/basic/hello-world.html?gi-language=python
+"""
+
 import os
 # Show error and log messages
 os.environ["GST_DEBUG"] = "2"
@@ -7,41 +13,44 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)s] - %(message)s")
 logger = logging.getLogger(__name__)
 
-
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
 
 
-# initialize GStreamer
-Gst.init(sys.argv[1:])
+def main():
+    # initialize GStreamer
+    Gst.init(sys.argv[1:])
 
-# build the pipeline
-pipeline = Gst.parse_launch(
-    "playbin uri=file:///app/videos/street_5min.mp4"
-)
+    # build the pipeline
+    pipeline = Gst.parse_launch(
+        "playbin uri=file:///app/videos/street_5min.mp4"
+    )
 
-# start playing
-pipeline.set_state(Gst.State.PLAYING)
+    # start playing
+    pipeline.set_state(Gst.State.PLAYING)
 
-# wait until EOS or error
-bus = pipeline.get_bus()
-msg = bus.timed_pop_filtered(
-    Gst.CLOCK_TIME_NONE,
-    Gst.MessageType.ERROR | Gst.MessageType.EOS
-)
-# Parse message
-if msg:
-    if msg.type == Gst.MessageType.ERROR:
-        err, debug_info = msg.parse_error()
-        logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
-        logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
-    elif msg.type == Gst.MessageType.EOS:
-        logger.info("End-Of-Stream reached.")
-    else:
-        # This should not happen as we only asked for ERRORs and EOS
-        logger.error("Unexpected message received.")
-    
-    
-# free resources
-pipeline.set_state(Gst.State.NULL)
+    # wait until EOS or error
+    bus = pipeline.get_bus()
+    msg = bus.timed_pop_filtered(
+        Gst.CLOCK_TIME_NONE,
+        Gst.MessageType.ERROR | Gst.MessageType.EOS
+    )
+    # Parse message
+    if msg:
+        if msg.type == Gst.MessageType.ERROR:
+            err, debug_info = msg.parse_error()
+            logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
+            logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
+        elif msg.type == Gst.MessageType.EOS:
+            logger.info("End-Of-Stream reached.")
+        else:
+            # This should not happen as we only asked for ERRORs and EOS
+            logger.error("Unexpected message received.")
+        
+    # free resources
+    pipeline.set_state(Gst.State.NULL)
+
+
+if __name__ == "__main__":
+    main()
